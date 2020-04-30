d1c1724677a5782de7caeaab6a5de1e77bba180b
Rakhibul-Hasan <64598908+Rakhibul-Hasan@users.noreply.github.com>
2020-05-01 03:56:57 +0600

    Create uri 2755 output 9.cxx

---
+++ uri 2755 output 9.cxx
@@ -0,0 +1,8 @@
+#include<stdio.h>
+int main()
+{
+	printf("%cRo'b'er\tto\\\/\%c\n",'"','"');
+	printf("(._.) ( l: ) ( .-. ) ( :l ) (._.)\n");
+	printf("(^_-) (-_-) (-_^)\n");
+	printf("(%c_%c) ('.')\n",'"','"');
+}
