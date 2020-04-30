c9da1ed0232279aa0cf145bff3ab77f6d22b46a1
Rakhibul-Hasan <64598908+Rakhibul-Hasan@users.noreply.github.com>
2020-05-01 03:58:28 +0600

    Create uri 2749 output 3.cxx

---
+++ uri 2749 output 3.cxx
@@ -0,0 +1,34 @@
+#include<stdio.h>
+int main()
+{
+ int i;
+ for(i=0;i<13;i++) printf("---");
+ printf("\n");
+ 
+ 	printf("|");
+ 	 printf("x = 35 ");
+ 	for(i=0;i<10;i++) printf("   ");
+ 	printf("|\n");
+ 	
+ printf("|");
+ 	for(i=0;i<37;i++) printf(" ");
+ 	printf("|\n");
+ 	
+ 	printf("|");
+ 	for(i=0;i<3;i++) printf("     ");
+ 	 printf("x = 35 ");
+ 	for(i=0;i<3;i++) printf("     ");
+ 	printf("|\n");
+ 	
+ 	printf("|");
+ 	for(i=0;i<37;i++) printf(" ");
+ 	printf("|\n");
+ 	
+ 	printf("|");
+ 	for(i=0;i<10;i++) printf("   ");
+ 	printf(" x = 35");
+ 	printf("|\n");
+ 	
+ for(i=0;i<13;i++) printf("---");
+ printf("\n");
+}
