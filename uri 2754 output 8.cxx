60178a4bcbf37c19930240caed45bbc02de2c52c
Rakhibul-Hasan <64598908+Rakhibul-Hasan@users.noreply.github.com>
2020-05-01 04:04:55 +0600

    Create uri 2754 output 8.cxx

---
+++ uri 2754 output 8.cxx
@@ -0,0 +1,6 @@
+#include<stdio.h>
+int main()
+{
+	double a=234.345,b=45.698;
+	 printf("%.6lf - %.6lf\n%.0lf - %.0lf\n%.1lf - %.1lf\n%.2lf - %.2lf\n%.3lf - %.3lf\n%e - %e\n%E - %E\n%.3lf - %.3lf\n%.3lf - %.3lf\n",a,b,a,b,a,b,a,b,a,b,a,b,a,b,a,b,a,b);
+}
