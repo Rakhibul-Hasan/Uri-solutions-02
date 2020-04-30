79de103f322282dabd134d68032ae4c80ee90686
Rakhibul-Hasan <64598908+Rakhibul-Hasan@users.noreply.github.com>
2020-05-01 03:59:52 +0600

    Create uri 2748 output 2.cxx

---
+++ uri 2748 output 2.cxx
@@ -0,0 +1,33 @@
+#include<stdio.h>
+int main()
+{
+ int i,j;
+ for(i=0;i<13;i++) printf("---");
+ printf("\n");
+ 
+ 	printf("|");
+ 	 printf("        Roberto");
+ 	for(i=0;i<11;i++) printf("  ");
+ 	printf("|\n");
+ 	
+ printf("|");
+ 	for(i=0;i<37;i++) printf(" ");
+ 	printf("|\n");
+ 	
+ 	printf("|");
+ 	 printf("        5786");
+ 	for(i=0;i<5;i++) printf("     ");
+ 	printf("|\n");
+ 	
+ 	printf("|");
+ 	for(i=0;i<37;i++) printf(" ");
+ 	printf("|\n");
+ 	
+ 	printf("|");
+ 	 printf("        UNIFEI ");
+ 	for(i=0;i<11;i++) printf("  ");
+ 	printf("|\n");
+ 	
+ for(i=0;i<13;i++) printf("---");
+ printf("\n");
+}
